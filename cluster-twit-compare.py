becbdcd3cd030f1efda26d04f1a5f3989a31a897
Caliatdrimme <sveta.sodol@gmail.com>
2020-04-22 22:17:25 -0700

    Add files via upload
    - improved code structure
    - added preprocessing of test data
    - added search for best number of cluster
    - added kmeans

--- cluster-twit-compare.py
+++ cluster-twit-compare.py
@@ -11,7 +11,9 @@ Created on Mon Apr 20 16:57:56 2020
 
 #fit with EM
 
-#fit with other models?
+#fit with k-means
+
+#fit with SOM
 
 #compare the models' fits
 
@@ -19,19 +21,42 @@ Created on Mon Apr 20 16:57:56 2020
 
 import itertools
 
+import math
+
 from sklearn import mixture
+from sklearn.cluster import KMeans
 
 from csv import reader
 from sklearn.feature_extraction.text import CountVectorizer
 
-def preprocess(file, column):
+
+def vect(train, test):
+   #takes in two arrays of strings
+   #vectorizes both from the dictionary formed from the first array
+   #returns both vectorized with the dictionary created
+   
+   #turn into bag of words representation
+   vectorizer = CountVectorizer()
+   
+   X = vectorizer.fit_transform(train)
+
+   nary = vectorizer.get_feature_names()
+   
+   res = X.toarray()
+   
+   test_res = vectorizer.transform(test)
+   
+   return res, nary, test_res.toarray()
+
+def preprocess(file_train, column, file_test, column_test):
    #takes in a file name (including path) of a csv file
    #and the column index of the column that contains the text of the tweets (0-based)
    #processes the tweets into bag of words representation for further processing
    #returns a vector of the processed tweets and a dictionary
 
+   #TRAINING DATA
    #read in the file as a list of lists
-   with open(file, 'r') as read_obj:
+   with open(file_train, 'r') as read_obj:
     # pass the file object to reader() to get the reader object
     csv_reader = reader(read_obj)
     # Pass reader object to list() to get a list of lists
@@ -42,51 +67,126 @@ def preprocess(file, column):
    original = []
    #my computer runs out of memory if doing full training dataset at once
    #so only with 10k for now
-   for i in range(1, 10000):
+   for i in range(1, 100):
       original.append(list_of_rows[i][column])
       
-   #turn into bag of words representation
-   vectorizer = CountVectorizer()
-   
-   X = vectorizer.fit_transform(original)
-
-   nary = vectorizer.get_feature_names()
-   
-   res = X.toarray()
+   #TEST DATA
+   #read in the file as a list of lists
+   with open(file_test, 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader_test = reader(read_obj)
+    # Pass reader object to list() to get a list of lists
+    list_of_rows_test = list(csv_reader_test)
+    
+   #extract the tweet texts only
+   #original is a list of all the tweets in original full-text form
+   original_test = []
+   #my computer runs out of memory if doing full training dataset at once
+   #so only with 10k for now
+   for i in range(1, 50):
+      original_test.append(list_of_rows_test[i][column_test])
       
-   return res, nary
+   res, nary, test = vect(original,original_test)
+         
+   return res, nary, test
 
 
-def fit_EM(data):
+def fit_EM(data, n):
    # fit a Gaussian Mixture Model with five components
 
-   gmm = mixture.GaussianMixture(n_components=5, covariance_type='full').fit(data)
+   gmm = mixture.GaussianMixture(n_components=n, covariance_type='full').fit(data)
    
    print("the model was fit\n")
    return gmm
+
+
+def EM(train, test, m):
+   
+   #finds the best EM model and returns the resulting cluster weights
+   
+   clusters = []
+   score = 0
+   n = 0
+   
+   #find best EM model 2 to m clusters
+   for i in range(2,m):
+      model = fit_EM(train,i)
+      #print(str(model.score(train)) + " the score of " + str(i) +" clusters \n")
+      #print("current best score is " + str(score) + "\n")
+      if model.score(train) > score:
+         clusters = model.means_
+         score = model.score(train)
+         n = i
+         best_model = model
+      
+   #print(model.predict(train))
+   print(clusters)
+   print(n)
+   print(score)
+   
+   print("testing score for EM is " + str(best_model.score(test)))
+   
+   return clusters
+
+def fit_kmeans(data, n):
+   # fit a Gaussian Mixture Model with five components
+
+   kmeans = KMeans(n_clusters=n, random_state=0).fit(data)
+   
+   #print("the model was fit\n")
+   return kmeans
+
+def Kmeans(train, test, m):
+   
+   #finds the best kmeans model and returns the resulting cluster weights
+   
+   clusters = []
+   score = -10000000000
+   n = 0
+   
+   #find best kmeans model 2 to m clusters
+   for i in range(2,m):
+      model = fit_kmeans(train,i)
+      #print(str(model.score(train)) + " the score of " + str(i) +" clusters \n")
+      #print("current best score is " + str(score) + "\n")
+      if model.score(train) > score:
+         clusters = model.cluster_centers_
+         score = model.score(train)
+         n = i
+         best_model = model
+      
+   #print(model.predict(train))
+   print(clusters)
+   print(n)
+   print(score)
+   
+   print("testing score for KMeans is " + str(best_model.score(test)))
+   
+   #best_model.predict(test)
+   
+   return clusters
 
    
 #constants definitions
 file_test = "test.csv"
 file_train = "train.csv"
 
-#preprocess the training data
-train, dict_train = preprocess(file_train, 2)
+#max number of clusters we want to find
+m = 5
 
-print(train[0])
+#preprocess the data
+train, dict_train, test = preprocess(file_train, 2, file_test, 1)
 
-model = fit_EM(train)
+#print(train[0])
 
-print(model.predict(train))
+em_res = EM(train, test, m+1)
 
+print(len(em_res[1]))
+print(len(dict_train))
 
-#print(dict_train)
+kmeans_res = Kmeans(train, test, m+1)
 
-
-
-#preprocess the test data
-#share the dictionary between the train and test datasets??
-#test, dict_test = preprocess(file_test, 2)
+#report results in nice graphics
 
 
 
